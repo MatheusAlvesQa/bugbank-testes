7f941ae0eda07f6ecec91306e2793a78d1dc801c
João Matheus <alvesmatheus1941@gmail.com>
2025-05-15 18:20:05 -0300

    Cypress configurado e projeto estruturado

--- testes-automatizados/cypress.config.js
+++ testes-automatizados/cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: "cypress/e2e/features/*.feature",
+    supportFile: false,
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
     },
   },
 });
